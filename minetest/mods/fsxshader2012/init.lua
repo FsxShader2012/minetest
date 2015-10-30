c14c3b20a4390ab2637ccdbe953d256c71a1b584
FsxShader2012 <robin@moonlightgamestg.de>
2015-10-30 13:18:30 +0100

    added fsxshader2012_rainbow block to fsxshader2012 mod

--- minetest/mods/fsxshader2012/init.lua
+++ minetest/mods/fsxshader2012/init.lua
@@ -1,11 +1,11 @@
---FsxShader2012 Block Test Mod ----------- Copyright Robin Kuhn 2015
+-- FsxShader2012 Block Test Mod ----------- Copyright Robin Kuhn 2015
 
 
 -- Define FsxShader2012 node
 minetest.register_node("fsxshader2012:fsxshader2012", {
 	description = "FsxShader2012 Block",
 	tile_images = {"fsxshader2012.bmp"},
-	groups = {stone=2, cracky=3, fall_damage_add_percent=-100, bouncy=150},
+	groups = {stone=2, cracky=3},
 	drop = {
 		items = {
 			{
@@ -19,6 +19,15 @@ minetest.register_node("fsxshader2012:fsxshader2012", {
 })
 
 
+-- Define FsxShader2012_rainbow node
+minetest.register_node("fsxshader2012:fsxshader2012_rainbow", {
+	description = "FsxShader2012 Rainbow Block",
+	tile_images = {"fsxshader2012_rainbow.png"},
+	groups = {stone=2, cracky=3, fall_damage_add_percent=-100, bouncy=150},
+	drop = '"fsxshader2012:fsxshader2012" 4'
+})
+
+
 -- Define crafting recipe for FsxShader2012 node
 minetest.register_craft({
 	output = '"fsxshader2012:fsxshader2012" 5',
@@ -27,4 +36,12 @@ minetest.register_craft({
 		{'default:diamond', 'default:mese_crystal', 'default:diamond', ''},
 		{'default:mese_crystal', 'default:diamond', 'default:mese_crystal', ''},
 		}
+})
+
+
+-- Define crafting recipe for _rainbow node
+minetest.register_craft({
+	type = "shapeless",
+	output = "fsxshader2012:fsxshader2012_rainbow",
+	recipe = {"fsxshader2012:fsxshader2012", "fsxshader2012:fsxshader2012", "fsxshader2012:fsxshader2012", "fsxshader2012:fsxshader2012"}
 })
